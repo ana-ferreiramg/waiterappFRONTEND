9c22132d0b538f66ac547ec6fce339bd7daf522e
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-06-15 13:37:05 -0300

    frontend: create interface for OrderModal and create your styles.ts

---
+++ src/components/OrderModal/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: rgba(0,0,0,0.8);
+  backdrop-filter: blur(0.45rem);
+  width: 100%;
+  height: 100%;
+`;
