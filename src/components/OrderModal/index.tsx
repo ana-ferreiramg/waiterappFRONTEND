b81cca2140c8fac4be4293867093372d9d8198c2
Ana Ferreira <anapaulaferreira0309@gmail.com>
2024-06-15 15:04:49 -0300

    frontend: create modal and your styles

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -1,17 +1,72 @@
-import { Overlay } from './styles';
+import closeIcon from '../../assets/images/close-icon.svg';
+import { Order } from '../../types/Order';
+import { formatCurency } from '../../utils/formatCurrency';
+
+import { Overlay, ModalBody, OrderDetails } from './styles';
 
 interface OrderModalProps {
   visible: boolean;
+  order: Order | null;
 }
 
-export function OrderModal({ visible }: OrderModalProps) {
-  if (!visible) {
+export function OrderModal({ visible, order }: OrderModalProps) {
+  if (!visible || !order) {
     return null;
   }
 
   return (
     <Overlay>
-      <h1>ORDER MODAL</h1>
+      <ModalBody>
+        <header>
+          <strong>Mesa {order.table}</strong>
+          <button type='button'>
+            <img src={closeIcon} alt="Icon to close modal" />
+          </button>
+        </header>
+
+        <div className="status-container">
+          <small>Status do Pedido</small>
+          <div>
+            <span>
+              {order.status === 'WAITING' && '🕐'}
+              {order.status === 'IN_PRODUCTION' && '🧑‍🍳'}
+              {order.status === 'DONE' && '✅'}
+            </span>
+
+            <strong>
+              {order.status === 'WAITING' && 'Fila de espera'}
+              {order.status === 'IN_PRODUCTION' && 'Em preparação'}
+              {order.status === 'DONE' && 'Pronto!'}
+            </strong>
+          </div>
+        </div>
+
+        <OrderDetails>
+          <strong>Itens</strong>
+
+          <div className="order-items">
+            {order.products.map(({ _id, product, quantity }) => (
+              <div className='item' key={_id}>
+                <img
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                  width='70'
+                  height='40'
+                />
+
+                <span className="quantity">{quantity}x</span>
+
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurency(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+        </OrderDetails>
+
+      </ModalBody>
     </Overlay>
   );
 }
--- src/components/OrderModal/styles.ts
+++ src/components/OrderModal/styles.ts
@@ -4,8 +4,103 @@ export const Overlay = styled.div`
   position: fixed;
   left: 0;
   top: 0;
+
   background: rgba(0,0,0,0.8);
   backdrop-filter: blur(0.45rem);
+
   width: 100%;
   height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ModalBody = styled.div`
+  background: #fff;
+  width: 48rem;
+  border-radius: 0.8rem;
+  padding: 3.2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+      font-size: 2.4rem;
+    }
+
+    button {
+      line-height: 0;
+      border: 0;
+      background: transparent;
+    }
+  }
+
+  .status-container {
+    margin-top: 3.2rem;
+
+    small {
+      font-size: 1.4rem;
+      opacity: 0.8rem;
+    }
+
+    div {
+      margin-top: 0.8rem;
+      display: flex;
+      align-items: center;
+      gap: 0.8rem;
+    }
+  }
+`;
+
+export const OrderDetails = styled.div`
+  margin-top: 3.2rem;
+
+  > strong {
+    font-weight: 500;
+    font-size: 1.4rem;
+    opacity: 0.8;
+  }
+
+  .order-items {
+    margin-top: 1.6rem;
+
+    .item {
+      display: flex;
+
+      & + .item {
+        margin-top: 1.6rem;
+      }
+
+      img {
+        border-radius: 0.6rem;
+      }
+
+      .quantity {
+         font-size: 1.4rem;
+         color: #666;
+
+         display: block;
+         min-width: 2rem;
+         margin-left: 1.2rem;
+      }
+
+      .product-details {
+
+        margin-left: 0.4rem;
+
+        strong {
+          display: block;
+          margin-bottom: 0.4rem;
+        }
+
+        span {
+          font-size: 1.4rem;
+          color: #666;
+        }
+      }
+    }
+  }
 `;
